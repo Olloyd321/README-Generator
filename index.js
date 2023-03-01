46b5062a60c3516c00f365b984304b7d9df47508
Olloyd321 <olloyd321@gmail.com>
2023-03-01 16:16:27 -0500

    added questions section, and started writing some functions

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
-// TODO: Include packages needed for this application
-
+const inquirer = require('inquirer') // inquirer package 
+const fs = require('fs') // file system package
+const generateMarkdown = require('./utils/generateMarkdown.js')
+console.log("Welcome to the readme generator, answer all prompts to render a readme") 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    // name of project 
+    {
+        type: 'input',
+        name: 'title',
+        message: "What would you like to name your project?"
+
+    },
+    // description of project
+    {
+        type: 'input',
+        name: 'description',
+        message: 'please provide a description of your project',
+    },
+    // usage of project
+    {
+
+    },
+    // license for project
+    {
+
+    },
+    // github username 
+    {
+
+    },
+    // constributors
+    {
+
+    },
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile (fileName, data)  
+    { 
+     console.log("ReadmMe generated")
+
+    };
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+}
 
 // Function call to initialize app
 init();
